3d2867a3766f2b22288ce6be90560653a3265642
Maxim <max@injectiveprotocol.com>
2021-01-12 10:30:45 +0300

    Add x/peggy types.

---
+++ orchestrator/sidechain/peggy/types/module.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	// ModuleName is the name of the module
+	ModuleName = "peggy"
+
+	// RouterKey is the module name router key
+	RouterKey = ModuleName
+
+	// QuerierRoute to be used for querierer msgs
+	QuerierRoute = ModuleName
+)
