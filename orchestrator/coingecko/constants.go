009f7f233f4d2f168ce746b43f3787cca4135e0d
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2022-02-16 13:38:23 +0100

    add umee address (#185)

--- orchestrator/coingecko/constants.go
+++ orchestrator/coingecko/constants.go
@@ -3,5 +3,5 @@ package coingecko
 // Everytime a new ERC20 is deployed in the Gravity Bridge contract, we need to
 // add it here and make a new release of Peggo.
 var bridgeTokensCoinIDs = map[string]string{
-	"0x0000000000000000000000000000000000000000": "eth",
+	"0xc0a4Df35568F116C370E6a6A6022Ceb908eedDaC": "umee",
 }
