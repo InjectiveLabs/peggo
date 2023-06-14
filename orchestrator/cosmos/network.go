afaf59f48ea2e8fd6ce9cc8a9a07237edf4ca495
Dusan Brajovic <dbrajovic3@gmail.com>
2023-06-14 13:43:01 +0200

    impl inj network for oracle

---
+++ orchestrator/cosmos/network.go
@@ -0,0 +1,44 @@
+package cosmos
+
+import (
+	"context"
+	peggyevents "github.com/InjectiveLabs/peggo/solidity/wrappers/Peggy.sol"
+
+	peggy "github.com/InjectiveLabs/sdk-go/chain/peggy/types"
+)
+
+type Network struct {
+	PeggyQueryClient
+	PeggyBroadcastClient
+}
+
+func NewNetwork() (*Network, error) {
+	return nil, nil
+}
+
+func (n *Network) PeggyParams(ctx context.Context) (*peggy.Params, error) {
+	return n.PeggyQueryClient.PeggyParams(ctx)
+}
+
+func (n *Network) LastClaimEvent(ctx context.Context) (*peggy.LastClaimEvent, error) {
+	return n.LastClaimEventByAddr(ctx, n.AccFromAddress())
+}
+
+func (n *Network) SendEthereumClaims(
+	ctx context.Context,
+	lastClaimEvent uint64,
+	oldDeposits []*peggyevents.PeggySendToCosmosEvent,
+	deposits []*peggyevents.PeggySendToInjectiveEvent,
+	withdraws []*peggyevents.PeggyTransactionBatchExecutedEvent,
+	erc20Deployed []*peggyevents.PeggyERC20DeployedEvent,
+	valsetUpdates []*peggyevents.PeggyValsetUpdatedEvent,
+) error {
+	return n.PeggyBroadcastClient.SendEthereumClaims(ctx,
+		lastClaimEvent,
+		oldDeposits,
+		deposits,
+		withdraws,
+		erc20Deployed,
+		valsetUpdates,
+	)
+}
