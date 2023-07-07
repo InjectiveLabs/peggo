f7c002f088aecaef83ff6180af3e03eeb598847c
Dusan Brajovic <dbrajovic3@gmail.com>
2023-07-07 11:25:45 +0200

    logging

--- orchestrator/oracle.go
+++ orchestrator/oracle.go
@@ -57,7 +57,7 @@ func (s *PeggyOrchestrator) EthOracleMainLoop(ctx context.Context) error {
 	}
 
 	return loops.RunLoop(ctx, defaultLoopDur, func() error {
-		logger.WithField("last_confirmed_eth_height", lastConfirmedEthHeight).Infoln("scanning for events")
+		logger.WithField("last_confirmed_eth_height", lastConfirmedEthHeight).Infoln("scanning for ethereum events")
 
 		// Relays events from Ethereum -> Cosmos
 		var currentHeight uint64
