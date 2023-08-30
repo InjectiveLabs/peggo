46de0e0f0897eab7631fc8703f7be6cff9d89fc9
Dusan Brajovic <dbrajovic3@gmail.com>
2023-08-30 12:35:04 +0200

    print

--- orchestrator/main_loops.go
+++ orchestrator/main_loops.go
@@ -3,6 +3,7 @@ package orchestrator
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math"
 	"math/big"
 	"time"
@@ -329,9 +330,14 @@ func (s *peggyOrchestrator) CheckFeeThreshold(erc20Contract common.Address, tota
 		return false
 	}
 
+	fmt.Printf("totalFee=%s minFeeInUSD=%v\n", totalFee.String(), minFeeInUSD)
+	fmt.Printf("totalFee(scaled)=%s tokenPriceInUSD=%s\n", decimal.NewFromBigInt(totalFee.BigInt(), -18).String(), decimal.NewFromFloat(tokenPriceInUSD).String())
+
 	tokenPriceInUSDDec := decimal.NewFromFloat(tokenPriceInUSD)
 	totalFeeInUSDDec := decimal.NewFromBigInt(totalFee.BigInt(), -18).Mul(tokenPriceInUSDDec)
 	minFeeInUSDDec := decimal.NewFromFloat(minFeeInUSD)
+
+	fmt.Printf("totalFeeInUSDDec=%s minFeeInUSDDec=%s\n", totalFeeInUSDDec.String(), minFeeInUSDDec.String())
 
 	if totalFeeInUSDDec.GreaterThan(minFeeInUSDDec) {
 		return true
