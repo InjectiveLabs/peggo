8bbd2736ae0bec3616d9b9a32b42fa7f74f1aa21
Dusan Brajovic <dbrajovic3@gmail.com>
2023-10-31 10:31:00 +0100

    start 3 peggo processes in parallel

--- test/peggo/deploy_bridge.sh
+++ test/peggo/deploy_bridge.sh
@@ -92,24 +92,98 @@ cd "${0%/*}" # cd in the script dir
 
 cwd=$(pwd)
 data_dir=$cwd/data
+example_env=$cwd/example.env
+localhost_tcp="tcp://localhost"
+localhost_http="http://localhost"
 
-n0_peggo_env=$data_dir/n0/.env
+
+
+n0_data_dir=$cwd/data/n0
+n0_peggo_env=$n0_data_dir/.env
 n0_eth_from="0x4e9feE2BCdf6F21b17b77BD0ac9faDD6fF16B4d4"
 n0_eth_pk="e85344fa1e00f06bd286b716e410ee0ad73541956c4cf59520f6db13599eb3f3"
-n0_cosmos_grpc="tcp://localhost:9090"
-n0_tendermint_rpc="http://localhost:26657"
+n0_cosmos_grpc="$localhost_tcp:9090"
+n0_tendermint_rpc="$localhost_http:26657"
 n0_cosmos_keyring="/Users/dbrajovic/Desktop/dev/Injective/peggo/test/cosmos/data/injective-333/n0"
 
 #n0_data_dir=$data_data/n0
 #n0_data_dir=$data_data/n0
 
-example_env=$cwd/example.env
 
-mkdir -p "$data_dir/n0"
+mkdir -p "$n0_data_dir"
 touch "$n0_peggo_env"
 
 sed -e "s|^PEGGO_ETH_FROM=.*|PEGGO_ETH_FROM=\"$n0_eth_from\"|" \
     -e "s|^PEGGO_ETH_PK=.*|PEGGO_ETH_PK=\"$n0_eth_pk\"|" \
+    -e "s|^PEGGO_COSMOS_KEYRING_DIR=.*|PEGGO_COSMOS_KEYRING_DIR=\"$n0_cosmos_keyring\"|" \
+    -e "s|^PEGGO_COSMOS_GRPC=.*|PEGGO_COSMOS_GRPC=\"$n0_cosmos_grpc\"|" \
+    -e "s|^PEGGO_TENDERMINT_RPC=.*|PEGGO_TENDERMINT_RPC=\"$n0_tendermint_rpc\"|" \
     "$example_env" > "$n0_peggo_env"
 
+
+
+
+n1_data_dir=$cwd/data/n1
+n1_peggo_env=$n1_data_dir/.env
+n1_eth_from="0xec43B0eA83844Cbe5A20F5371604BD452Cb1012c"
+n1_eth_pk="60f6ee19454b8ff45693cd54c55860785e4af9eeb06d6c5617568458e4ca5c54"
+n1_cosmos_keyring="/Users/dbrajovic/Desktop/dev/Injective/peggo/test/cosmos/data/injective-333/n1"
+n1_cosmos_grpc="$localhost_tcp:9091"
+n1_tendermint_rpc="$localhost_http:26667"
+
+#n0_data_dir=$data_data/n0
+#n0_data_dir=$data_data/n0
+
+
+mkdir -p "$n1_data_dir"
+touch "$n1_peggo_env"
+
+sed -e "s|^PEGGO_ETH_FROM=.*|PEGGO_ETH_FROM=\"$n1_eth_from\"|" \
+    -e "s|^PEGGO_ETH_PK=.*|PEGGO_ETH_PK=\"$n1_eth_pk\"|" \
+    -e "s|^PEGGO_COSMOS_KEYRING_DIR=.*|PEGGO_COSMOS_KEYRING_DIR=\"$n1_cosmos_keyring\"|" \
+    -e "s|^PEGGO_COSMOS_GRPC=.*|PEGGO_COSMOS_GRPC=\"$n1_cosmos_grpc\"|" \
+    -e "s|^PEGGO_TENDERMINT_RPC=.*|PEGGO_TENDERMINT_RPC=\"$n1_tendermint_rpc\"|" \
+    "$example_env" > "$n1_peggo_env"
+
+
+
+n2_data_dir=$cwd/data/n2
+n2_peggo_env=$n2_data_dir/.env
+n2_eth_from="0x8B094eD440900CEB75B83A22eD8A2C7582B442C2"
+n2_eth_pk="21eeff959d9752704e3f1ad6562fd0458c003bce0947e5aecf07b602f4e457aa"
+n2_cosmos_keyring="/Users/dbrajovic/Desktop/dev/Injective/peggo/test/cosmos/data/injective-333/n2"
+n2_cosmos_grpc="$localhost_tcp:9092"
+n2_tendermint_rpc="$localhost_http:26677"
+
+#n0_data_dir=$data_data/n0
+#n0_data_dir=$data_data/n0
+
+
+mkdir -p "$n2_data_dir"
+touch "$n2_peggo_env"
+
+sed -e "s|^PEGGO_ETH_FROM=.*|PEGGO_ETH_FROM=\"$n2_eth_from\"|" \
+    -e "s|^PEGGO_ETH_PK=.*|PEGGO_ETH_PK=\"$n2_eth_pk\"|" \
+    -e "s|^PEGGO_COSMOS_KEYRING_DIR=.*|PEGGO_COSMOS_KEYRING_DIR=\"$n2_cosmos_keyring\"|" \
+    -e "s|^PEGGO_COSMOS_GRPC=.*|PEGGO_COSMOS_GRPC=\"$n2_cosmos_grpc\"|" \
+    -e "s|^PEGGO_TENDERMINT_RPC=.*|PEGGO_TENDERMINT_RPC=\"$n2_tendermint_rpc\"|" \
+    "$example_env" > "$n2_peggo_env"
+
+
+# Start a new tmux session
+tmux new-session -d -s mysession
+
+# Split the terminal vertically into three equally spaced panes
+tmux split-window -v
+tmux split-window -v
+tmux select-layout even-vertical
+
+# Select each pane and run a command from a different directory
+peggo_cmd="peggo orchestrator"
+tmux send-keys -t 0 "cd $n0_data_dir" C-m "$peggo_cmd" C-m
+tmux send-keys -t 1 "cd $n1_data_dir" C-m "$peggo_cmd" C-m
+tmux send-keys -t 2 "cd $n2_data_dir" C-m "$peggo_cmd" C-m
+
+# Attach to the tmux session to view the processes
+tmux attach-session -t mysession
 
