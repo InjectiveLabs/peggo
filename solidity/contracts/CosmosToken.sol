9f6a1e95220c1e78332ec646aaabfd541f58be52
Markus Waas <mail@markuswaas.com>
2024-06-13 10:30:15 +0200

    feat: add adhoc minting for native tokens on Injective

--- solidity/contracts/CosmosToken.sol
+++ solidity/contracts/CosmosToken.sol
@@ -3,22 +3,31 @@
 pragma solidity ^0.8.0;
 
 import "./@openzeppelin/contracts/ERC20.sol";
+import "./@openzeppelin/contracts/Ownable.sol";
 
-contract CosmosERC20 is ERC20 {
-	uint256 MAX_UINT = 2**256 - 1;
-	uint8 immutable private _decimals;
+contract CosmosERC20 is ERC20, Ownable {
+    uint256 MAX_UINT = 2 ** 256 - 1;
+    uint8 private immutable _decimals;
 
-	constructor(
-		address peggyAddress_,
-		string memory name_,
-		string memory symbol_,
-		uint8 decimals_
-	) ERC20(name_, symbol_) {
-		_decimals = decimals_;
-		_mint(peggyAddress_, MAX_UINT);
-	}
+    constructor(
+        address peggyAddress_,
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+        _mint(peggyAddress_, MAX_UINT);
+    }
 
-	function decimals() public view virtual override returns (uint8) {
-		return _decimals;
-	}
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
 }
